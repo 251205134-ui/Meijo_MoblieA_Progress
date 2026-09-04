04ec3accb13a0b7027a05ed6a4d9a994a8c07c5a
Mizuki Kato <213+mzk@users.noreply.github.com>
2026-09-04 11:33:20 +0900

    ActivityLauncherの設定

--- app/src/main/java/jp/ac/meijou/android/meijo_mobliea_progress/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/meijo_mobliea_progress/MainActivity.java
@@ -1,8 +1,11 @@
 package jp.ac.meijou.android.meijo_mobliea_progress;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,11 +20,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK -> {
+
+                    }
+                    case RESULT_CANCELED -> {
+
+                    }
+                    default -> {
+
+                    }
+                }
+            }
+    );
 }
