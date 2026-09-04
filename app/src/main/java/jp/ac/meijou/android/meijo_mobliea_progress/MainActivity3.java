8a16bf2ea1bff7d3cdfe2cdb29987e1ae57bf803
YahhoWorld <taikikimura07090709@gmail.com>
2026-09-04 14:51:39 +0900

    view3との簡単な接続

--- app/src/main/java/jp/ac/meijou/android/meijo_mobliea_progress/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/meijo_mobliea_progress/MainActivity3.java
@@ -1,5 +1,6 @@
 package jp.ac.meijou.android.meijo_mobliea_progress;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.meijo_mobliea_progress.databinding.ActivityMain3Binding;
+
 public class MainActivity3 extends AppCompatActivity {
+    private ActivityMain3Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main3);
+//        setContentView(R.layout.activity_main3);
+        binding=ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.addProgressButton.setOnClickListener(view->{
+            var name=binding.addProgressEditText.getText().toString();
+            if(name.isEmpty()||name.isBlank())return;
+            var intent=new Intent();
+            intent.putExtra("name",name);
+            setResult(RESULT_OK,intent);
+            finish();
+            
+
         });
     }
 }
